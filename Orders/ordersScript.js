953769c867d4761b5007cc2bd64a1cedfec5bc16
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-11-04 18:37:28 +0700

    Страница деталей заказ

--- Orders/ordersScript.js
+++ Orders/ordersScript.js
@@ -1,6 +1,6 @@
 data = 
 {
-  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJhYmJjNWQ4YS1jYjU4LTRjMjYtOTA5ZC0wOGRiZDZhNzIxYjkiLCJuYW1lIjoibmFtZS5zdXJuYW1lQGRvbWFpbi5jb20iLCJlbWFpbCI6Im5hbWUuc3VybmFtZUBkb21haW4uY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvYXV0aGVudGljYXRpb24iOiIzNTJhY2ZlZi00NDUwLTRiNmYtYTcxNi02NzMwNmMwNjc1OGQiLCJuYmYiOjE2OTkwMjk2MjYsImV4cCI6MTY5OTAzMzIyNiwiaWF0IjoxNjk5MDI5NjI2LCJpc3MiOiJEZWxpdmVyeS5BcGkiLCJhdWQiOiJEZWxpdmVyeS5BcGkifQ.wJDfJzmsnGIkqfKYOSD_n3YXsa_qWeNxQLkmcao0KGg"
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJhYmJjNWQ4YS1jYjU4LTRjMjYtOTA5ZC0wOGRiZDZhNzIxYjkiLCJuYW1lIjoibmFtZS5zdXJuYW1lQGRvbWFpbi5jb20iLCJlbWFpbCI6Im5hbWUuc3VybmFtZUBkb21haW4uY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvYXV0aGVudGljYXRpb24iOiJkNWY3YzQwMy1lM2FhLTQ3MDAtOWMyMi1jMjJmMTY5NzMzODgiLCJuYmYiOjE2OTkwOTQxOTQsImV4cCI6MTY5OTA5Nzc5NCwiaWF0IjoxNjk5MDk0MTk0LCJpc3MiOiJEZWxpdmVyeS5BcGkiLCJhdWQiOiJEZWxpdmVyeS5BcGkifQ.inJrblUxwZtQrGpNhwK2EBVzYSMhpyCf8ZF-JSZTGj4"
 }
 const url = "https://food-delivery.kreosoft.ru/api/order";
 const token = "Bearer " + data.token;
@@ -17,12 +17,11 @@ function handleResponse(data) {
     data.forEach(order => {
         const row = document.createElement("tr");
         row.innerHTML = `
-            <td>${order.deliveryTime}</td>
+            <td><a href="C:/Users/kfk55/.vscode/WebFront-Hits-frontend-project-1/Orders/detailOrder.html?orderId=${order.id}"><u>${order.deliveryTime}</u></a></td>
             <td>Заказ от ${new Date(order.orderTime).toLocaleTimeString()}</td>
             <td>${order.status === "InProcess" ? "В обработке" : "Доставлен"}</td>
             <td>${order.price}</td>
         `;
-
         if (order.status === "InProcess") {
             const confirmButton = document.createElement("button");
             confirmButton.innerText = "Подтвердить доставку";
