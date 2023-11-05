a20a6458a19df79ac7490c4fdc8dbb384b76b27b
Дмитрий Фофанцев <kfk.551@yandex.ru>
2023-11-05 23:56:39 +0700

    добавление в корзину, не работает селектор

--- Menu/menuScript.js
+++ Menu/menuScript.js
@@ -44,6 +44,7 @@ function fetchData(page) {
                             <p class="card-text">Категория: ${dish.category}</p>
                             <p class="card-text">Рейтинг: ${stars}</p>
                             <a href="C:/Users/kfk55/.vscode/WebFront-Hits-frontend-project-1/Dish/dishHTML.html?id=${dish.id}" class="btn btn-primary">Подробнее</a>
+                            <button class="btn btn-success add-to-cart" data-dish-id="${dish.id}">В корзину</button>
                         </div>
                     </div>
                 </div>
@@ -97,3 +98,38 @@ document.querySelectorAll('.card').forEach(img => {
         window.location.href = `/Dish/dishHTML.html?id=${productId}`;
     });
 });
+
+function addToCart(dishId) {
+    console.log("addToCart")
+    const data = {
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJhYmJjNWQ4YS1jYjU4LTRjMjYtOTA5ZC0wOGRiZDZhNzIxYjkiLCJuYW1lIjoibmFtZS5zdXJuYW1lQGRvbWFpbi5jb20iLCJlbWFpbCI6Im5hbWUuc3VybmFtZUBkb21haW4uY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvYXV0aGVudGljYXRpb24iOiJmNzZkZmFkOS0wM2FlLTRjYmItYmI5OS1kOWQxYmUyMDgyZTIiLCJuYmYiOjE2OTkyMDI0MzksImV4cCI6MTY5OTIwNjAzOSwiaWF0IjoxNjk5MjAyNDM5LCJpc3MiOiJEZWxpdmVyeS5BcGkiLCJhdWQiOiJEZWxpdmVyeS5BcGkifQ.VvU1JnHznjGsRGdO4b2jHFPycZ6eVNaNwz72YWqBxXU",
+    };
+
+    fetch(`https://food-delivery.kreosoft.ru/api/basket/dish/${dishId}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+    })
+    .then((response) => {
+        if (response.status === 200) {
+        } else {
+            console.error("Не удалось добавить товар в корзину.");
+        }
+    })
+    .catch((error) => {
+        console.error("Произошла ошибка при выполнении запроса: " + error);
+    });
+}
+document.querySelectorAll(".add-to-cart").forEach((button) => {
+    button.addEventListener("click", (event) => {
+        console.log("addToBasket");
+        const dishId = button.getAttribute("data-dish-id");
+        console.log("addToBasket");
+        addToCart(dishId);
+    });
+});
+
+
